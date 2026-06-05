7db9edccb30a3178bae12918afde69c8634e5aa4
Kartik Gundla <207908131+kartikgundla@users.noreply.github.com>
2026-06-05 12:04:26 +0530

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,10 +1,3 @@
-// import { createClient } from '@supabase/supabase-js'
-
-// const supabaseUrl = 'https://kybamgqjwnolxcpnlumm.supabase.co'
-// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt5YmFtZ3Fqd25vbHhjcG5sdW1tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MzE5NjcsImV4cCI6MjA5NjIwNzk2N30.QzUJebG8pRdbRMpZ1r7RpxXzvaBYHcXpaY8Jph-UKtU'
-
-// export const supabase = createClient(supabaseUrl, supabaseKey)
-
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
